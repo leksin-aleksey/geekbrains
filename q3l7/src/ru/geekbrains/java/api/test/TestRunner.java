9c649bf505bf0a885df3d809b01c8c8cb5fd9945
sbt-leksin-aa <AALeksin.SBT@sberbank.ru>
2020-10-21 15:19:04 +0300

    it works

--- q3l7/src/ru/geekbrains/java/api/test/TestRunner.java
+++ q3l7/src/ru/geekbrains/java/api/test/TestRunner.java
@@ -1,8 +1,11 @@
 package ru.geekbrains.java.api.test;
 
+import ru.geekbrains.java.tests.CustomTest1;
+
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
@@ -13,15 +16,16 @@ public class TestRunner {
 
     private static Map<Integer, List<Method>> methodsOrder = new TreeMap<>();
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception{
+//        CustomTest1 test1 = new CustomTest1();
+        start(CustomTest1.class);
     }
 
     private TestRunner() {}
 
     public static void start(Class<?> clazz) throws Exception{
-        Constructor<?> constructor = clazz.getConstructor(clazz);
-        Object object = constructor.newInstance("");
+        Constructor<?> constructor = clazz.getConstructor();
+        Object object = constructor.newInstance();
         checkMethodExistanceOnceAsMax(object, BeforeSuite.class);
         checkMethodExistanceOnceAsMax(object, AfterSuite.class);
         collectMethods(object);
@@ -53,13 +57,21 @@ public class TestRunner {
     private static void collectMethods(Object object){
         for (Method method : object.getClass().getMethods()) {
             for (Annotation a : method.getDeclaredAnnotations()) {
-                    if (a.annotationType() == BeforeSuite.class){
-                        methodBeforeSuite = method;
-                    } else if (a.annotationType() == AfterSuite.class){
-                        methodAfterSuite = method;
-                    } else if (a.annotationType() == Test.class){
-                        /*TODO*/
+                if (a.annotationType() == BeforeSuite.class){
+                    methodBeforeSuite = method;
+                } else if (a.annotationType() == AfterSuite.class){
+                    methodAfterSuite = method;
+                } else if (a.annotationType() == Test.class){
+                    /*TODO test method order*/
+                    List<Method> list;
+                    if (!methodsOrder.containsKey(1)){
+                        list = new LinkedList<>();
+                    } else {
+                        list = methodsOrder.get(1);
                     }
+                    list.add(method);
+                    methodsOrder.put(1, list);
+                }
             }
         }
     }
@@ -67,14 +79,19 @@ public class TestRunner {
     private static void runMethods(Object object) throws Exception{
         if (methodBeforeSuite != null){
             /*TODO parameters*/
-            methodBeforeSuite.invoke("");
+            methodBeforeSuite.invoke(object, "");
         }
 
-
+        for (Map.Entry<Integer, List<Method>> entry : methodsOrder.entrySet()){
+            /*TODO parameters*/
+            for(Method method : entry.getValue()){
+                method.invoke(object);
+            }
+        }
 
         if (methodAfterSuite != null){
             /*TODO parameters*/
-            methodAfterSuite.invoke("");
+            methodAfterSuite.invoke(object,"");
         }
     }
 }
--- q3l7/src/ru/geekbrains/java/tests/CustomTest1.java
+++ q3l7/src/ru/geekbrains/java/tests/CustomTest1.java
@@ -5,23 +5,25 @@ import ru.geekbrains.java.api.test.AfterSuite;
 import ru.geekbrains.java.api.test.Test;
 
 public class CustomTest1 {
-    @BeforeSuite
-    public void doFirst(){
+    public CustomTest1(){}
 
+    @BeforeSuite
+    public void doFirst(String s){
+        System.out.println("prepare");
     }
 
     @Test
     public void doTest1(){
-
+        System.out.println("test1");
     }
 
     @Test
     public void doTest2(){
-
+        System.out.println("test2");
     }
 
     @AfterSuite
-    public void diLast(){
-
+    public void doLast(String s){
+        System.out.println("finish");
     }
 }
